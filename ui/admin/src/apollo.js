a3696774558f98a4e5c865df93b031c2768fe4b8
Manos Aloupis <e.aloupis@covis.de>
2021-04-22 17:03:48 +0300

    fix env variables for admin

--- ui/admin/src/apollo.js
+++ ui/admin/src/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const getApolloClient = () =>
   new ApolloClient({
-    uri: `https://patmos-gateway.herokuapp.com/graphql`,
+    uri: process.env.REACT_APP_GATEWAY ? `${process.env.REACT_APP_GATEWAY}/graphql` : `https://patmos-nginx.herokuapp.com/gateway/graphql`,
     credentials: 'include',
     cache: new InMemoryCache(),
   });
